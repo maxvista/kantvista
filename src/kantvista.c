d21f9fb3485e67e5261eb1c47dcc3f3fa6560df1
maxvista <77590375+maxvista@users.noreply.github.com>
2021-02-05 03:57:27 +0100

    Alle Umlaute entfernt :(

--- src/kantvista.c
+++ src/kantvista.c
@@ -5,18 +5,18 @@
 #include "word.h"
 
 int main () {
-        srand((unsigned)time(NULL));
-        int i;
-        printf("Drücke 1 für Vista.\n");
-        printf("Drücke 2 für Kant.\n");
-        scanf("%i", &i);
-        if (i == 1) {
-                int n = rand() % 723;
-                printf ("%svista\n", w[n]);
-        } else if (i == 2) {
-                int n = rand() % 723;
-                printf ("%skant\n", w[n]);
-        } else {
-                printf("Nicht gültige Eingabe.\n");
-        }
+    srand((unsigned)time(NULL));
+    int i;
+    printf("Druecke 1 fuer Vista.\n");
+    printf("Druecke 2 fuer Kant.\n");
+    scanf("%i", &i);
+    if (i == 1) {
+            int n = rand() % 723;
+            printf ("%svista\n", w[n]);
+    } else if (i == 2) {
+            int n = rand() % 723;
+            printf ("%skant\n", w[n]);
+    } else {
+            printf("Nicht gueltige Eingabe.\n");
+    }
 }
